5c48343a3d6a9426a898420c2019f18fec79224e
jelipo <me@jelipo.com>
2020-10-04 23:55:33 +0800

    fix

--- src/bit/usize.rs
+++ src/bit/usize.rs
@@ -6,7 +6,7 @@ pub trait BitUtil<T> {
     /// ```
     /// use fantasy_util::bit::usize::BitUtil;
     ///
-    /// let a = 0b1001_1001;
+    /// let a:u32 = 0b1001_1001 as u32;
     /// let i:u32 = a.extract(3,4); // i = 0b0000_0011;
     /// ```
     fn extract(self, off: u8, len: u8) -> T;
@@ -18,18 +18,17 @@ pub trait BitUtil<T> {
     /// use fantasy_util::bit::usize::BitUtil;
     ///
     /// let a:u32 = 0b1001_1001 as u32;
-    /// let b:bool = a.get_bit_bool(0); // b = true;
+    /// let b:bool = a.get_bit_bool(0); // b=true;
     /// ```
-    fn get_bit_bool(self, bit: u8) -> bool { self.get_bit(bit) != 0 }
+    fn get_bit_bool(self, bit: u8) -> bool;
 
     /// 获取指定位
-    ///
     /// #Example
     /// ```
     /// use fantasy_util::bit::usize::BitUtil;
     ///
     /// let a:u32 = 0b1001_1001 as u32;
-    /// let b:u8 = a.get_bit(0); // b = 1;
+    /// let b:bool = a.get_bit_bool(0); // b = true;
     /// ```
     fn get_bit(self, bit: u8) -> u8;
 }
@@ -37,6 +36,7 @@ pub trait BitUtil<T> {
 impl BitUtil<u8> for u8 {
     fn extract(self, off: u8, len: u8) -> u8 { (self >> off) & ((1u8 << len) - 1) }
 
+    fn get_bit_bool(self, bit: u8) -> bool { self.get_bit(bit) != 0 }
 
     fn get_bit(self, bit: u8) -> u8 { ((self >> bit) as u8) & 1 }
 }
@@ -44,6 +44,7 @@ impl BitUtil<u8> for u8 {
 impl BitUtil<u16> for u16 {
     fn extract(self, off: u8, len: u8) -> u16 { (self >> off) & ((1u16 << len) - 1) }
 
+    fn get_bit_bool(self, bit: u8) -> bool { self.get_bit(bit) != 0 }
 
     fn get_bit(self, bit: u8) -> u8 { ((self >> bit) as u8) & 1 }
 }
@@ -51,6 +52,8 @@ impl BitUtil<u16> for u16 {
 
 impl BitUtil<u32> for u32 {
     fn extract(self, off: u8, len: u8) -> u32 { (self >> off) & ((1u32 << len) - 1) }
+
+    fn get_bit_bool(self, bit: u8) -> bool { self.get_bit(bit) != 0 }
 
     fn get_bit(self, bit: u8) -> u8 { ((self >> bit) as u8) & 1 }
 }
